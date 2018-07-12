c0c1bd8fc827cab796de9a40c2ab2e081191b2f2
chrisvasqm <cvasquez@oriontek.do>
2018-07-11 20:15:44 -0400

    Add the array of students

--- src/main/kotlin/com/chrisvasqm/Main.kt
+++ src/main/kotlin/com/chrisvasqm/Main.kt
@@ -1,5 +1,11 @@
 package com.chrisvasqm
 
 fun main(args: Array<String>) {
-    println("Hello, World")
+    val students = arrayOf(
+            Student("Chris", 80),
+            Student("Mark", 67),
+            Student("Joseph", 98),
+            Student("Carl", 74),
+            Student("Katherine", 86)
+    )
 }
